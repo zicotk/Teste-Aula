2cc9d89e5602001b9db7354ab5e0cbbab895fa4c
Rodrigo <zico.oliveira162@gmail.com>
2022-04-09 00:24:13 +0000

    feat: criação do programa de teste

---
+++ src/fabricacelular/Bateria.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fabricacelular;
+
+/**
+ *
+ * @author francisleide
+ */
+public class Bateria {
+    private String marca;
+    private String modelo;
+    private float voltagem;
+    private int capacidade;
+
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(float voltagem) {
+        this.voltagem = voltagem;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+   
+    
+}
---
+++ src/fabricacelular/Programa.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fabricacelular;
+
+/**
+ *
+ * @author francisleide
+ */
+public class Programa {
+    private String nome;
+    private float espacoOcupado;
+    private String objetivo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getEspacoOcupado() {
+        return espacoOcupado;
+    }
+
+    public void setEspacoOcupado(float espacoOcupado) {
+        this.espacoOcupado = espacoOcupado;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String objetivo) {
+        this.objetivo = objetivo;
+    }
+    
+    
+}
